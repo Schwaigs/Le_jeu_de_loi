c724b1c8ee90d986d12482009436f199956407e9
Axel <agrimmer@etu.unistra.fr>
2019-10-21 02:31:55 +0200

    Ajout du js

---
+++ js/index.js
@@ -0,0 +1,13 @@
+/******************************************************************************
+ * SCRIPT INDEX
+ ******************************************************************************/
+
+ /* Open when someone clicks on the span element */
+ function ouvreArbre() {
+   document.getElementById("arbreDepl").style.width = "66%";
+ }
+
+ /* Close when someone clicks on the "x" symbol inside the overlay */
+ function fermeArbre() {
+   document.getElementById("arbreDepl").style.width = "0%";
+ }
