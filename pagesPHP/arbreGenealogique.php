2f5085708515ae55869653cef9e050f727cce4c2
Axel <agrimmer@etu.unistra.fr>
2019-12-13 12:42:04 +0100

    Changement arbre de place suite au bug

--- pagesPHP/arbreGenealogique.php
+++ pagesPHP/arbreGenealogique.php
@@ -1,4 +1,5 @@
 <?php
+session_start();
 require_once '../accesBDD/classesPHP/Arbre.php';
 
 
@@ -9,6 +10,13 @@ try{
   //La méthode initArbre créer l'arbre généalogique sous forme de liste en HTML.
   // elle initialise la racine puis lance la création de chaque fraterie de manière récursive.
     $arbre->initArbre();
+    if (!isset($_GET['refresh'])){
+     /*
+     * \var refresh est une variable de session qui permet de rafraichir la page lors de chaque action.
+     */
+     header('Location: arbreGenealogique.php?refresh=0');
+     exit();
+   }
 }
 catch( PDOException $e ) {
     echo 'Erreur : '.$e->getMessage();
